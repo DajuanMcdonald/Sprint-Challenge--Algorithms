75289da4be6276a579fde79ee0aaee131ecc60a9
Dajuan Mcdonald <flopwiki@gmail.com>
2020-07-20 21:16:51 -0400

    fixed test case for casesentative

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,16 +4,26 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 
+# this is a O(n), input influences the count but not the runtime
+
 
 def count_th(word):
-    if len(word) <= 2:
+    if len(word) < 2:
         return 0
 
     occur = word[:2]
-    if occur == "th" or occur == "TH":
+    if occur == "th" or occur == "TH" and occur == "tH" and occur == "Th":
         return 1 + count_th(word[2:])
 
     return count_th(word[1:])
 
+# prints 6
 
-print(count_th("testhisthatthTHosethemthree"))
+
+print(count_th(word="testhisthatthTHosethemthree"))
+
+# prints 1
+print(count_th(word="leather"))
+
+# prints 1
+print(count_th(word="th"))
